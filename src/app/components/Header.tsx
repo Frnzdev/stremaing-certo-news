394f610fc7bea59fb886434487c34c275cc25ca2
frnzdev <felipeacfornazeiro@gmail.com>
2025-05-22 12:11:38 -0300

    arrumando as coisas

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
     <header className="mt-4 ml-5 mr-5 bg-gray-200 py-6  rounded-2xl">
       <div className="flex items-center gap-8 px-6">
         <Image src="/conecta.png" alt="Logo" width={240} height={39} />
-        <Tabs defaultValue="account" className="p-2 bg-slate-100 rounded-md">
+        <Tabs defaultValue="Esporte" className="p-2 bg-slate-100 rounded-md">
           <TabsList>
             <TabsTrigger
               className="pt-3 pb-3 px-4 py-4 focus:bg-white focus:border-none"
